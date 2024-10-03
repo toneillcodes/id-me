0c7e8ae73e569eff3c6e4e9c08946ff429621654
Tom O'Neill <tom.oneill13@gmail.com>
2024-10-03 19:13:20 -0400

    Updating ipaddress to remote_address, adjusting and fixing output formats, added file logging

--- whoami.php
+++ whoami.php
@@ -1,9 +1,25 @@
 <?php
 
+// configuration settings
+$logdata = false;
+$logfile = "/var/www/html/idme/userinfo.csv";
+
 // collect data
-$ipaddress = $_SERVER['REMOTE_ADDR'] ?: "na";
+$remote_address = $_SERVER['REMOTE_ADDR'] ?: "na";
 $x_fwd_for = $_SERVER['HTTP_X_FORWARDED_FOR'] ?: "na";
 $http_client_ip = $_SERVER['HTTP_CLIENT_IP'] ?: "na";
+
+if($logdata) {
+	// format the current time
+	$t=time();
+	$ts = date("Y-m-d,H:i:s",$t);
+
+	// open the log file
+	$logfile = fopen($logfile, "a") or die("Unable to open logfile!");
+	// record entry, one per line
+	fwrite($logfile, $ts . ',' . $remote_address . "," . $x_fwd_for . "," . $http_client_ip . PHP_EOL);
+	fclose($logfile);
+}
 
 // encode and output
 $encoding = $_GET['e'];
@@ -20,33 +36,39 @@ image/jpeg
 
 if(isset($encoding)) {
 	if($encoding == "html") {
-		  header('Content-Type: text/html');
-		  echo "<html><body><span name='ra'>" . $ipaddress . "</span><span name='xff'>" . $x_fwd_for . "</span><span name='hxi'>" . $http_client_ip . "</body></html>";
+		header('Content-Type: text/html');
+		echo "<html><body><span name='ra'>" . $remote_address . "</span><span name='xff'>" . $x_fwd_for . "</span><span name='hxi'>" . $http_client_ip . "</body></html>";
     } else if($encoding == "css") {
   		header('Content-Type: text/css');
-  		echo "body .ra { background-color: " . $ipaddress . "; } ";
+  		echo "body .ra { background-color: " . $remote_address . "; } ";
   		echo "div .xff { background-color: " . $x_fwd_for . "; }";
   		echo "span .hci { background-color: " . $http_client_ip . "; }";
     } else if($encoding == "csv") {
 		header('Content-Type: text/csv');
-  		echo "ip," . $ipaddress . PHP_EOL;
+  		echo "ra," . $remote_address . PHP_EOL;
   		echo "xff," . $x_fwd_for . PHP_EOL;
   		echo "hci," . $http_client_ip . PHP_EOL;
     } else if($encoding == "js") {
   		header('Content-Type: text/javascript');
-  		echo "document.ready(function() {" . $ipaddress . "});";
-    } else if($encoding == "js") {
+  		echo "document.ready(function() {";
+		echo "let ra=" . $remote_address . ";";
+		echo "let xff= " . $x_fwd_for . ";";
+		echo "let hci=" . $http_client_ip . ";";
+		echo "});";
+    } else if($encoding == "txt") {
   		header('Content-Type: text/plain');
-  		echo "ip_address: " . $ipaddress . "});";
+		echo "ra:" . $remote_address . ";";
+		echo "xff:" . $x_fwd_for . ";";
+		echo "hci:" . $http_client_ip . ";";
 	} else {
   		header('Content-Type: application/json');
-  		$userinfo = array("ip"=>$ipaddress,"xff"=>$x_fwd_for,"hci"=>$http_client_ip);
+  		$userinfo = array("ra"=>$remote_address,"xff"=>$x_fwd_for,"hci"=>$http_client_ip);
   		echo json_encode($userinfo);
     }
 } else {
-  header('Content-Type: application/json');
-  $userinfo = array("ip"=>$ipaddress,"xff"=>$x_fwd_for,"hci"=>$http_client_ip);
-  echo json_encode($userinfo);
+  	header('Content-Type: application/json');
+  	$userinfo = array("ra"=>$remote_address,"xff"=>$x_fwd_for,"hci"=>$http_client_ip);
+	echo json_encode($userinfo);
 }
 
 ?>
